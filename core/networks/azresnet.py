9c03fa5221757144ecd2c74160f6f8fcf795bced
Xavier Valcarce <xvalcarce@protonmail.ch>
2024-12-20 16:01:55 +0100

    add: parametrised resnet

--- core/networks/azresnet.py
+++ core/networks/azresnet.py
@@ -14,20 +14,25 @@ class AZResnetConfig:
     policy_head_out_size: int
     num_blocks: int
     num_channels: int
+    num_policy_channels: int = 1
+    num_value_channels: int = 2
+    batch_norm_momentum: float = 1.0
+    kernel_size: int = 3
 
 
 class ResidualBlock(nn.Module):
     """Residual block for AlphaZero ResNet model.
     - `channels`: number of channels"""
     channels: int
+    momentum: float
 
     @nn.compact
     def __call__(self, x, train: bool):
-        y = nn.Conv(features=self.channels, kernel_size=(3,3), strides=(1,1), padding='SAME', use_bias=False)(x)
-        y = nn.BatchNorm(use_running_average=not train)(y)
+        y = nn.Conv(features=self.channels, kernel_size=(self.kernel_size,self.kernel_sizel), strides=(1,1), padding='SAME', use_bias=False)(x)
+        y = nn.BatchNorm(momentum=self.momentum, use_running_average=not train)(y)
         y = nn.relu(y)
-        y = nn.Conv(features=self.channels, kernel_size=(3,3), strides=(1,1), padding='SAME', use_bias=False)(y)
-        y = nn.BatchNorm(use_running_average=not train)(y)
+        y = nn.Conv(features=self.channels, kernel_size=(self.kernel_size,self.kernel_size), strides=(1,1), padding='SAME', use_bias=False)(y)
+        y = nn.BatchNorm(momentum=self.momentum, use_running_average=not train)(y)
         return nn.relu(x + y)
 
 
@@ -39,23 +44,24 @@ class AZResnet(nn.Module):
     @nn.compact
     def __call__(self, x, train: bool):
         # initial conv layer
-        x = nn.Conv(features=self.config.num_channels, kernel_size=(3,3), strides=(1,1), padding='SAME', use_bias=False)(x)
-        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.Conv(features=self.config.num_channels, kernel_size=(self.kernel_size,self.kernel_size), strides=(1,1), padding='SAME', use_bias=False)(x)
+        x = nn.BatchNorm(momentum=self.config.batch_norm_momentum, use_running_average=not train)(x)
         x = nn.relu(x)
 
         # residual blocks
         for _ in range(self.config.num_blocks):
-            x = ResidualBlock(channels=self.config.num_channels)(x, train=train)
+            x = ResidualBlock(channels=self.config.num_channels, momentum=self.config.batch_norm_momentum)(x, train=train)
 
         # policy head
-        policy = nn.Conv(features=2, kernel_size=(1,1), strides=(1,1), padding='SAME', use_bias=False)(x)
-        policy = nn.BatchNorm(use_running_average=not train)(policy)
+        policy = nn.Conv(features=self.config.num_policy_channels, kernel_size=(1,1), strides=(1,1), padding='SAME', use_bias=False)(x)
+        policy = nn.BatchNorm(momentum=self.config.batch_norm_momentum, use_running_average=not train)(policy)
         policy = nn.relu(policy)
         policy = policy.reshape((policy.shape[0], -1))
         policy = nn.Dense(features=self.config.policy_head_out_size)(policy)
+        policy = nn.softmax(policy)
 
         # value head
-        value = nn.Conv(features=1, kernel_size=(1,1), strides=(1,1), padding='SAME', use_bias=False)(x)
+        value = nn.Conv(features=self.config.num_value_channels, kernel_size=(1,1), strides=(1,1), padding='SAME', use_bias=False)(x)
         value = nn.BatchNorm(use_running_average=not train)(value)
         value = nn.relu(value)
         value = value.reshape((value.shape[0], -1))
